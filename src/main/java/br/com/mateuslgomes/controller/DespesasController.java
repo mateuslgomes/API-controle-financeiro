d56c9f937d589a2d0132d4c109d138d6dbe97ffc
Mateus Lopes Gomes <97681752+mateuslgomes@users.noreply.github.com>
2022-08-23 21:26:27 -0300

    GET - Detalhamento de despesa por id

--- src/main/java/br/com/mateuslgomes/controller/DespesasController.java
+++ src/main/java/br/com/mateuslgomes/controller/DespesasController.java
@@ -2,18 +2,17 @@ package br.com.mateuslgomes.controller;
 
 import br.com.mateuslgomes.controller.dto.DespensaDto;
 import br.com.mateuslgomes.model.Despensas;
+import br.com.mateuslgomes.model.Receitas;
 import br.com.mateuslgomes.repository.DespensasRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/despesas")
@@ -21,6 +20,12 @@ public class DespesasController {
 
     @Autowired
     DespensasRepository despensasRepository;
+
+    @RequestMapping(path = "{id}")
+    public ResponseEntity<Despensas> despensa(@PathVariable Long id) {
+        Optional<Despensas> receita = despensasRepository.findById(id);
+        return receita.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
 
     @PostMapping
     public ResponseEntity<Despensas> saveDespensa(@RequestBody @Valid DespensaDto dto, UriComponentsBuilder uriBuilder) {
